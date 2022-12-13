a4e5cec5efb1c6d23d8ff697e2dde5b875123daa
abhinav-neil <abhinav.neil@gmail.com>
2022-12-13 07:02:40 +0100

    cleanup

--- unittests.py
+++ unittests.py
@@ -1,20 +1,3 @@
-################################################################################
-# MIT License
-#
-# Copyright (c) 2022
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to conditions.
-#
-# Author: Deep Learning Course | Fall 2022
-# Date Created: 2022-11-25
-################################################################################
-
-
 import unittest
 import numpy as np
 import torch
